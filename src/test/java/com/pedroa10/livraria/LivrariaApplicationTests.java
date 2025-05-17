d2a5e28fc659864269fc52a639478d1820fca65f
Pedro-A10 <pedroaugusto732@gmail.com>
2025-05-17 19:14:14 -0300

    Projeto criado

---
+++ src/test/java/com/pedroa10/livraria/LivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroa10.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
