25a214a563068f070ce0b83f780d5962b2bfcdbb
Pedro-A10 <pedroaugusto732@gmail.com>
2025-05-17 20:34:54 -0300

    Classes Author e Book criadas

---
+++ src/main/java/com/pedroa10/livraria/model/Author.java
@@ -0,0 +1,57 @@
+package com.pedroa10.livraria.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "authors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String bio;
+	private LocalDate birthDate;
+	private String nationality;
+	
+	@OneToMany(mappedBy = "author")
+	private List<Book> books;
+
+	public Author(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+}
---
+++ src/main/java/com/pedroa10/livraria/model/Book.java
@@ -0,0 +1,57 @@
+package com.pedroa10.livraria.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String isbn;
+	private Double price;
+	private String genre;
+	private LocalDate pubDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "author_id")
+	private Author author;
+	
+	public Book(String tittle) {
+		this.title = tittle;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+}
